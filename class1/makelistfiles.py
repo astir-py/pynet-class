9f8c76a72e04721765550880552e1f6d806899d9
astir-py <gh@stirton.org>
2017-08-05 03:23:05 -0700

    Added YAML default_flow_style=False

--- class1/makelistfiles.py
+++ class1/makelistfiles.py
@@ -11,7 +11,7 @@ xlist[-1]["ip_address"] = "172.16.22.22"
 xlist[-1]["attribs"] = range(8)
 
 with open("xfile.yml" , "w") as f:
-    yaml.dump(xlist, f)
+    yaml.dump(xlist, f, default_flow_style=False)
 
 with open("xfile.json" , "w") as f:
     json.dump(xlist, f)
